fd49a6bea284bb1ae1fc59a874a31018f6871f88
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-26 08:24:09 +0600

    added a Load data on route using loader and useLoaderData

---
+++ src/Components/User/User.jsx
@@ -0,0 +1,23 @@
+
+
+const User = ({user}) => {
+
+    const {id, name, email, phone} = user
+    const userStyle = {
+        border: "2px solid green",
+        padding: "5px",
+        borderRadius: "20px",
+        backgroundColor: "white",
+         color: "red"
+
+    }
+    return (
+        <div style={userStyle}>
+            <h2>{name}</h2>
+            <p>email: {email}</p>
+            <p>phone: {phone}</p>
+        </div>
+    );
+};
+
+export default User;
